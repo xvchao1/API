31c60ae7a1aacabafb70b1be195af7b361226e1b
xuanqiyan <17310920903@163.com>
2018-09-19 09:07:24 +0800

    数据标准化输出完毕

--- client/client.php
+++ client/client.php
@@ -1,2 +1,25 @@
 <?php
+
 	//客户端发起api请求
+	include  './vendor/autoload.php';
+
+	//请求的api地址
+	$url = 'http://localhost/ruanjian_02/api/server/server.php';
+
+
+	$curl = new Curl\Curl();
+	$curl->get($url);
+
+	if ($curl->error) {
+	    echo $curl->error_code;
+	}
+	else {
+	    echo $curl->response;
+	}
+
+	
+	
+
+
+
+?>
--- server/server.php
+++ server/server.php
@@ -1,3 +1,25 @@
 <?php
-	//接受请求 响应数据  json
-?>
+	//接受请求 响应数据  json	
+
+	$pdo =  new PDO('mysql:host=localhost;dbname=api;charset=utf8','root','');
+
+	$stmt = $pdo->query('select * from user');
+
+	$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+		
+	//定义标准化产出数据格式函数
+	function resp($data,$status,$message){
+	
+		$res = [
+			'status'=>$status,//服务器响应状态码  200  401 404
+			'message'=>$message,//此次api请求的描述  
+			'data'=>$data
+		];
+
+		return json_encode($res,JSON_UNESCAPED_UNICODE);
+	}					
+	
+	echo resp($data,200,'ok');	
+?>	
