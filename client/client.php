b485506f5184f804385f10f6c8f3aea60bd42c72
xc <2304333599@qq.com>
2018-09-20 07:36:59 +0800

    数据标准化输出

--- client/client.php
+++ client/client.php
@@ -1,3 +1,17 @@
-<?php 
+<?php
 	//客户端发起API请求
+	include './vendor/autoload.php';
+	//请求的api地址
+	$url='http://localhost/API/server/server.php';
+
+	$curl = new Curl\Curl();
+	$curl->get($url);
+	if ($curl->error) {
+	    echo $curl->error_code;
+	}
+	else {
+	    echo $curl->response;
+	}
+
+	
  ?>
--- server/server.php
+++ server/server.php
@@ -1,3 +1,21 @@
 <?php 
 	//接受请求并相应数据 json格式
+	$pdo=new PDO('mysql:host=localhost;dbname=api;charset=utf8','root','');
+
+	$stmt=$pdo->query('select * from user');
+
+	$data=$stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+	//定义标准化数据格式
+	function resp($data,$status,$message){
+		$res=[
+			'status'=>$status,// 服务器响应状态码 200 401 404
+			'message'=>$message,// 此次API请求的描述
+			'data'=>$data
+		];
+		return json_encode($res,JSON_UNESCAPED_UNICODE);
+	}
+
+	echo resp($data,200,'ok');
  ?>
